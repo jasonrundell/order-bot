c31eac37b6a752ea0fd08edc4d9095a2e48585e6
Jason Rundell <jason.rundell@gmail.com>
2018-11-25 10:33:54 -0500

    Rearrange

--- src/Bot.js
+++ src/Bot.js
@@ -4,10 +4,10 @@ const AI = require('./AI');
 
 const Bot = (function(){
   const CONFIG = {
+    BOT_ID: 'BDNNQPXUK',
     BOT_TOKEN: 'xoxb-2184034487-464264402385-um4Okes6DTaopoorwUP4KEj7',
     BOT_NAME: 'order-bot',
     BOT_FRIENDLY_NAME: 'Order Bot',
-    BOT_ID: 'BDNNQPXUK',
     BOT_EMOJI: ':robot_face:',
     COMPANY_NAME: 'Lucy\'s Coffee',
     COMPANY_NAME_PLURAL: 'Lucy\'s Coffee\'s'
