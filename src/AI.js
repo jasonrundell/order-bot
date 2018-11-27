6fc2c93ee538e510b789765328c2842808a3d8bc
Jason Rundell <jason.rundell@gmail.com>
2018-11-27 14:15:17 -0500

    State and identity events

--- src/AI.js
+++ src/AI.js
@@ -7,23 +7,29 @@ const AI = (function(){
 
   const queryResponses = {
     respondWithGreeting: [
-        'hi',
-        'hello',
-        'yo',
-        'hey',
-        'bonjour',
-        'wassup',
-        'howdy'
+      'hi',
+      'hello',
+      'yo',
+      'hey',
+      'bonjour',
+      'wassup',
+      'howdy'
     ],
     respondWithHoursOfOperation: [
-        'hours',
-        'what are your hours of operation',
-        'when do you open',
-        'are you open'
+      'hours',
+      'what are your hours of operation',
+      'when do you open',
+      'are you open'
     ],
     respondWithHelp: [
         'help',
-        'what can you do'
+        'help me',
+        'i need help',
+        'i need some help',
+        'i need a little help',
+        'what can you help with',
+        'what can you do',
+        'what do you do'
     ],
     respondWithBalance: [
       'balance',
@@ -33,6 +39,18 @@ const AI = (function(){
       'what is my balance',
       'do i have anything',
       'do i have a balance'
+    ],
+    respondWithState: [
+      'open',
+      'state',
+      'are you open',
+      'are you open right now'
+    ],
+    respondWithIdentity: [
+      'identify',
+      'identify yourself',
+      'who are you',
+      'what are you'
     ]
   };
 
@@ -77,6 +95,12 @@ const AI = (function(){
         case 'respondWithBalance': {
           return AI.respondWithBalance();
         }
+        case 'respondWithState': {
+          return AI.respondWithState();
+        }
+        case 'respondWithIdentity': {
+          return AI.respondWithIdentity();
+        }
       }
     },
     respondWithMisunderstanding: function(querySent='') {
@@ -103,10 +127,30 @@ const AI = (function(){
       return `Our hours this week are:\nMon - Thurs: 7:30 am - 4 pm\nFri: 7:30 am - 2:30 pm`;
     },
     respondWithHelp: function() {
-      return `Current commands I am familiar with are:\n\`hours\` - I will give you the store hours.\n\`balance\` - I will tell you your remaning balance.`;
+      return `Current commands I am familiar with are:
+      \n\`hours\` - I will give you the store hours.
+      \n\`balance\` - I will tell you your remaning balance.
+      \n\`open\` - I will tell you if Taddle Creek is open.
+      \n\`identify\` - I will tell you my purpose.
+      `;
     },
     respondWithBalance: function() {
       return `You have $25.00 CAD remaining.`;
+    },
+    respondWithState: function() {
+      let date = new Date();
+      let currentHour = parseInt(date.getHours());
+      let response = `Ask me again later.`;
+      if (currentHour >= 7 && currentHour <= 16) {
+        response = `We're open!`;
+      } else {
+        response = `We're currently closed. We re-open at 7:30 am.`;
+      }
+
+      return response;
+    },
+    respondWithIdentity: function() {
+      return `I'm the Taddle Creek Slack Bot! You can ask me questions or find out more by asking me for \`help\``;
     }
   };
 })();
